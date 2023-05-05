fb16f7a674d116eb1eabe1cd5de5d0fb0b5138b3
santiago.herrera <santiago.herrera@gmail.com>
2023-05-05 14:29:18 -0300

    MR: create members.json, set up memberRouter

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import cors from 'cors';
 // use "require" to import JSON files
 const admins = require('./data/admins.json');
 
+const member = require('./data/member.json')
+const membersRouter = require('./resources/member');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/member', membersRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
--- src/resources/member.js
+++ src/resources/member.js
@@ -1,4 +1,17 @@
-const arrowFunc = (array) => {
-  const arrayPusheado = array.pop('ejemplo')
-  return arrayPusheado
- }
+const express = require('express');
+
+const membersRouter = express.Router();
+
+const members = require('../data/member.json');
+
+membersRouter.get('/', (req, res) => res.send(members));
+
+membersRouter.get('/:id', (req, res) => {
+  const memberId = req.params.id;
+  const foundMember = members.find(member => member.id.toString() === memberId);
+  (foundMember) ? res.send(foundMember) : res.send('Member not found');
+});
+
+
+
+module.exports = membersRouter;
