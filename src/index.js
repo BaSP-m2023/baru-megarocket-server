5008293f278b744db312a48bfa263a676987ec62
moradpablo <moradpabloivan@gmail.com>
2023-05-06 12:42:21 -0300

    MR: add fixes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import cors from 'cors';
 
 // use "require" to import JSON files
 const admins = require('./data/admins.json');
-
-const member = require('./data/member.json')
 const membersRouter = require('./resources/member');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
--- src/resources/member.js
+++ src/resources/member.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const membersRouter = express.Router();
 
@@ -8,18 +9,32 @@ membersRouter.get('/', (req, res) => res.send(members));
 
 membersRouter.get('/:id', (req, res) => {
   const memberId = req.params.id;
-  const foundMember = members.find(member => member.id.toString() === memberId);
-  (foundMember) ? res.send(foundMember) : res.send('Member not found');
+  const foundMember = members.find((member) => member.id.toString() === memberId);
+  return foundMember ? res.send(foundMember) : res.send('Member not found');
 });
 
+membersRouter.post('/', (req, res) => {
+  const newMember = req.body;
+  members.push(newMember);
+  fs.writeFile('src/data/member.json', JSON.stringify(members, null, 2), (err) => {
+    if (err) {
+      res.send('Error!: member cannot be created');
+    } else {
+      res.send('Member created');
+    }
+  });
+});
+
+membersRouter.delete('/:id', (req, res) => {
+  const memberId = req.id;
+  const filteredMembers = members.filter((member) => member.id.toString() !== memberId);
+  fs.writeFile('./src/data/member.json', JSON.stringify(filteredMembers, null, 2), (err) => {
+    if (err) {
+      res.send('Error!. Member cannot be deleted');
+    } else {
+      res.send('Member deleted');
+    }
+  });
+});
 
 module.exports = membersRouter;
-// ------------------------------------------------------------------------------
-const fs = require('fs');
-membersRouter.delete('/:id' , (req, res) => {
-  const memberId = req.id;
-  const filteredMembers = members.filter(member => member.id.toString() !== memberId);
-  fs.writeFile('./src/data/member.json', JSON.stringify(filteredMembers, null, 2), err => {
-    (err) ? res.send('Error!. Member cannot be deleted') : res.send('Member deleted');
-  } )
-});
