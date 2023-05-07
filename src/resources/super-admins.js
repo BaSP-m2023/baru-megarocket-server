7691a5e934516ba8bf259372a2c4673b5a49e280
Cristian Zufiaurre <cjzufi@gmail.com>
2023-05-07 15:47:17 -0300

    MR: adding delete method

--- src/resources/super-admins.js
+++ src/resources/super-admins.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const superAdminRouter = express.Router();
 const superAdmins = require('../data/super-admins.json');
@@ -9,6 +10,24 @@ superAdminRouter.get('/:id', (req, res) => {
   const searchId = superAdmins.some((superAdmin) => superAdmin.id.toString() === req.params.id);
   if (searchId) {
     res.json(superAdmins.filter((superAdmin) => superAdmin.id.toString() === req.params.id));
+  } else {
+    res.status(400).json({ msg: `Super Admin with id of ${req.params.id} not found` });
+  }
+});
+
+superAdminRouter.delete('/:id', (req, res) => {
+  const searchId = superAdmins.some((superAdmin) => superAdmin.id.toString() === req.params.id);
+  if (searchId) {
+    const newSuperAdmins = superAdmins.filter(
+      (superAdmin) => superAdmin.id.toString() !== req.params.id,
+    );
+    fs.writeFile('../data/super-admins.json', JSON.stringify(newSuperAdmins, null, 2), (err) => {
+      if (!err) {
+        res.status(400).json({ msg: 'Error' });
+      } else {
+        res.json(newSuperAdmins);
+      }
+    });
   } else {
     res.status(400).json({ msg: `Super Admin with id of ${req.params.id} not found` });
   }
