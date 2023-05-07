9d99392cacdfd1e85352d6aee19781d3248f8335
gabiholl <gabrielahollmann@gmail.com>
2023-05-07 17:12:15 -0300

    MR:get and post done

--- src/resources/activity.js
+++ src/resources/activity.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const activities = require('../data/activity.json');
 
@@ -16,6 +17,18 @@ router.get('/:id', (req, res) => {
   } else {
     res.send('Activity not found!');
   }
+});
+
+router.post('/post', (req, res) => {
+  const newActivity = req.body;
+  activities.push(newActivity);
+  fs.writeFile('src/data/activity.json', JSON.stringify(activities, null, 2), (err) => {
+    if (err) {
+      res.send('Error. Activity cannot be created!');
+    } else {
+      res.send('User created');
+    }
+  });
 });
 
 module.exports = router;
