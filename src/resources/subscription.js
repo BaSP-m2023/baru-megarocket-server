b1e1bf50fb172b927cce9d5cb845519af3e6e05f
Nicolas Simonetti <racingclub_nico@hotmail.com>
2023-05-09 15:23:51 -0300

    MR: get by filter added, put status request added, get/del/post methods deleted, better mocked data on the json

--- src/resources/subscription.js
+++ src/resources/subscription.js
@@ -4,30 +4,6 @@ const subscriptions = require('../data/subscription.json');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  const newSub = req.body;
-  subscriptions.push(newSub);
-  fs.writeFile('src/data/subscription.json', JSON.stringify(subscriptions, null, 2), (e) => {
-    if (e) {
-      res.send('Error, can not be created');
-    } else {
-      res.send('Subscription created');
-    }
-  });
-});
-
-router.delete('/:id', (req, res) => {
-  const subId = req.params.id;
-  const filteredSubs = subscriptions.filter((sub) => sub.id.toString() !== subId);
-  fs.writeFile('src/data/subscription.json', JSON.stringify(filteredSubs, null, 2), (e) => {
-    if (e) {
-      res.send('Error, can not be deleted');
-    } else {
-      res.send('Subscription deleted');
-    }
-  });
-});
-
 router.put('/:id', (req, res) => {
   const subId = req.params.id;
   const found = subscriptions.some((sub) => sub.id.toString() === subId);
@@ -51,11 +27,17 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.get('/', (req, res) => {
+router.get('/filter', (req, res) => {
+  const param = req.query.class;
+  const filteredSubs = subscriptions.filter((sub) => sub.class.name === param);
   try {
-    res.send(subscriptions);
+    if (param) {
+      res.status(200).send(filteredSubs);
+    } else {
+      throw new Error();
+    }
   } catch (e) {
-    res.send('Error, can not get the element');
+    res.status(400).send('Error, can not get the element');
   }
 });
 
