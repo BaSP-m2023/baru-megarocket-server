683de717435080b6a156328d9fda83d77a0331b5
Nicolas Simonetti <racingclub_nico@hotmail.com>
2023-05-09 14:45:04 -0300

    MR: minor changes in subscription spcript, clean index code

--- src/index.js
+++ src/index.js
@@ -3,27 +3,17 @@ import express from 'express';
 import cors from 'cors';
 
 // use "require" to import JSON files
-const admins = require('./data/admins.json');
 const subscriptions = require('./resources/subscription');
 
-const member = require('./data/member.json')
-const membersRouter = require('./resources/member');
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 app.use('/subscriptions', subscriptions);
-app.use('/member', membersRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-});
-
-app.get('/admins', (req, res) => {
-  res.status(200).json({
-    data: admins,
-  });
 });
 
 app.listen(port, () => {
--- src/resources/subscription.js
+++ src/resources/subscription.js
@@ -4,6 +4,30 @@ const subscriptions = require('../data/subscription.json');
 
 const router = express.Router();
 
+router.post('/', (req, res) => {
+  const newSub = req.body;
+  subscriptions.push(newSub);
+  fs.writeFile('src/data/subscription.json', JSON.stringify(subscriptions, null, 2), (e) => {
+    if (e) {
+      res.send('Error, can not be created');
+    } else {
+      res.send('Subscription created');
+    }
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const subId = req.params.id;
+  const filteredSubs = subscriptions.filter((sub) => sub.id.toString() !== subId);
+  fs.writeFile('src/data/subscription.json', JSON.stringify(filteredSubs, null, 2), (e) => {
+    if (e) {
+      res.send('Error, can not be deleted');
+    } else {
+      res.send('Subscription deleted');
+    }
+  });
+});
+
 router.put('/:id', (req, res) => {
   const subId = req.params.id;
   const found = subscriptions.some((sub) => sub.id.toString() === subId);
@@ -11,18 +35,19 @@ router.put('/:id', (req, res) => {
     const updSub = req.body;
     subscriptions.forEach((sub) => {
       if (sub.id.toString() === subId) {
+        // eslint-disable-next-line no-param-reassign
         sub.class = updSub.class;
       }
     });
     fs.writeFile('src/data/subscription.json', JSON.stringify(subscriptions, null, 2), (e) => {
       if (e) {
-        res.send('Error, can not be modified');
+        res.status(500).send('Error, can not be modified');
       } else {
-        res.send('Subscription updated');
+        res.status(200).send('Subscription updated');
       }
     });
   } else {
-    res.status(400).send('Subscription does not exist');
+    res.status(404).send('Subscription does not exist');
   }
 });
 
