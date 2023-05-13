9a290a3c8f9335c9c88e4aa6b5224c4a28145157
Nicolas Simonetti <racingclub_nico@hotmail.com>
2023-05-13 15:44:00 -0300

    MR-13: basic controllers added for get and modify, models modified

--- src/controllers/subscription.js
+++ src/controllers/subscription.js
@@ -1,69 +1,59 @@
-const express = require('express');
-const fs = require('fs');
-const subscriptions = require('../data/subscription.json');
+const Subscription = require('../models/Subscription');
 
-const router = express.Router();
-
-router.put('/:id', (req, res) => {
-  const subId = req.params.id;
-  const found = subscriptions.some((sub) => sub.id.toString() === subId);
-  if (found) {
-    const updSub = req.body;
-    subscriptions.forEach((sub) => {
-      if (sub.id.toString() === subId) {
-        // eslint-disable-next-line no-param-reassign
-        sub.class = updSub.class;
-      }
+const getAllSubs = (req, res) => {
+  Subscription.find()
+    .then((subscriptions) => {
+      return res.status(200).json({
+        message: "All subscriptions list",
+        data: subscriptions,
+        error: false
+      });
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        message: "Internal error",
+        error
+      });
     });
-    fs.writeFile('src/data/subscription.json', JSON.stringify(subscriptions, null, 2), (e) => {
-      if (e) {
-        res.status(500).send('Error, can not be modified');
-      } else {
-        res.status(200).send('Subscription updated');
-      }
+};
+
+const getSubById = (req, res) => {
+  const { id } = req.params;
+
+  Subscription.findById(id)
+    .then((subscription) => {
+      return res.status(200).json({
+        message: 'Subscription found',
+        data: subscription,
+        error: false
+      });
+    })
+    .catch((error) => {
+      return res.json({
+        message: "Internal error",
+        error
+      });
     });
-  } else {
-    res.status(404).send('Subscription does not exist');
-  }
-});
+};
 
-router.get('/filter', (req, res) => {
-  const param = req.query.class;
-  const filteredSubs = subscriptions.filter((sub) => sub.class.name === param);
-  try {
-    if (param) {
-      res.status(200).send(filteredSubs);
-    } else {
-      throw new Error();
-    }
-  } catch (e) {
-    res.status(400).send('Error, can not get the element');
-  }
-});
+const updateSub = (req, res) => {
+  const { id } = req.params;
+  const { classes } = req.body;
 
-router.post('/', (req, res) => {
-  const newSubscription = req.body;
-  subscriptions.push(newSubscription);
-  fs.writeFile('src/data/subscription.json', JSON.stringify(subscriptions, null, 2), (err) => {
-    if (err) {
-      res.send('Error: subscription could not be created!');
-    } else {
-      res.send('Subscription created successfully!');
-    }
-  });
-});
-
-router.delete('/:id', (req, res) => {
-  const deleteSub = req.params.id;
-  // eslint-disable-next-line max-len
-  const filteredSubscriptions = subscriptions.filter((sub) => sub.id && sub.id.toString() !== deleteSub);
-  fs.writeFile('src/data/subscription.json', JSON.stringify(filteredSubscriptions, null, 2), (err) => {
-    if (err) {
-      res.send('Error: subscription could not be deleted!');
-    } else {
-      res.send('Subscription deleted successfully');
-    }
-  });
-});
-
-module.exports = router;
+  Subscription.findByIdAndUpdate(
+    id,
+    { classes },
+    { new: true }
+  )
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          message: '${id} Subscription was not found'
+        });
+      }
+      return res.status(200).json(result);
+    })
+    .catch((error) => {
+      return res.status(400).json(error);
+    });
+};
---
+++ src/models/subscription.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const subSchema = new Schema({
+  classes: {
+    type: Object
+  }
+});
+
+module.exports = mongoose.model('Subscription', subSchema);
