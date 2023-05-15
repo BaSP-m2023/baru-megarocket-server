52969e01fa6461a9c146131a7d41c58e3ab639d7
Alejandra Paggiola <alefpaggiola@gmail.com>
2023-05-15 20:39:20 -0300

    MR-11: delete and find by ID created

--- src/routes/trainer.js
+++ src/routes/trainer.js
@@ -8,6 +8,8 @@ const trainerValidation = require('../validations/trainer');
 
 trainerRouter.get('/', trainerController.getTrainers)
   .post('/', trainerValidation.validateTrainerCreate, trainerController.createTrainer)
-  .put('/:id', trainerValidation.validateTrainerUpdate, trainerController.updateTrainer);
+  .put('/:id', trainerValidation.validateTrainerUpdate, trainerController.updateTrainer)
+  .delete('/:id', trainerController.deleteTrainer)
+  .get('/:id', trainerController.getTrainerById);
 
 module.exports = trainerRouter;
