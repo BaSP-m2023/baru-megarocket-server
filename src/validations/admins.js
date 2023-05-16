20db34f5fb4166dc4903d57c2fec2e02b6ac134b
LautaroVercellini <lautarovercellini@gmail.com>
2023-05-16 20:15:19 -0300

    MR-#5: modified postman collection in admins and validations

--- src/validations/admins.js
+++ src/validations/admins.js
@@ -33,10 +33,8 @@ const validateCreation = (req, res, next) => {
 
 const validateUpdate = (req, res, next) => {
   const adminValidation = Joi.object({
-    firstName: Joi.string().pattern(/^[a-zA-Z]+$/).min(3).max(20)
-      .required(),
-    lastName: Joi.string().pattern(/^[a-zA-Z]+$/).min(3).max(20)
-      .required(),
+    firstName: Joi.string().pattern(/^[a-zA-Z]+$/).min(3).max(20),
+    lastName: Joi.string().pattern(/^[a-zA-Z]+$/).min(3).max(20),
     dni: Joi.number().min(10000000).max(100000000).integer()
       .positive(),
     phone: Joi.number().min(1000000000).max(10000000000).integer()
