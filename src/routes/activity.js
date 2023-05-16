d0b2d848ec0d045cc238ecf715ba80a5b33d1c3c
gabiholl <gabrielahollmann@gmail.com>
2023-05-16 18:50:48 -0300

    MR-03:fixed requested errors

--- src/routes/activity.js
+++ src/routes/activity.js
@@ -5,10 +5,10 @@ const validations = require('../validations/activity');
 const router = express.Router();
 
 router
-/*   .get('/', activityController.getAllActivity)
-  .get('/:id', activityController.getActivityById) */
+  .get('/', activityController.getAllActivity)
+  .get('/:id', activityController.getActivityById)
   .post('/', validations.validateCreation, activityController.createActivity)
-  .delete('/:id', activityController.deleteActivity);
-/*   .put('/:id', validations.validateUpdateActivity, activityController.updateActivity); */
+  .delete('/:id', activityController.deleteActivity)
+  .put('/:id', validations.validateUpdateActivity, activityController.updateActivity);
 
 module.exports = router;
