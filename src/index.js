81d49a978bfd4f9ba5ef4b5361be24ea3af3696e
BareaMartin <martin.barea94@gmail.com>
2023-05-18 09:56:19 -0300

    MR-XX: Updated master branch for DB and GIT purposes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 4000;
 const generalRoute = require('./routes/index');
 
 mongoose
-  .connect('mongodb+srv://baru-team:x60lbGF6arRYrycR@megarocket-databases.inpprte.mongodb.net/baru-database')
+  .connect('mongodb+srv://baru-team:x60lbGF6arRYrycR@megarocket-databases.inpprte.mongodb.net/baru-database', { maxPoolSize: process.env.MONGO_POOLSIZE || 1 })
   // eslint-disable-next-line no-console
   .then(() => console.log('Connected to MR DB'))
   // eslint-disable-next-line no-console
