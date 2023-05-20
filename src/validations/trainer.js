05a913cc6065dbf660a907dcb27cc6b1543ce27b
Mauro Jimenez <maurojim123@gmail.com>
2023-05-20 01:50:35 -0300

    MR-33: Modifications on trainers.js in Validation's folder

--- src/validations/trainer.js
+++ src/validations/trainer.js
@@ -4,11 +4,13 @@ const validateTrainerCreate = (req, res, next) => {
   const trainerValidation = Joi.object({
     firstName: Joi.string().pattern(/^[a-zA-Z\s]+$/).min(3).required(),
     lastName: Joi.string().pattern(/^[a-zA-Z\s]+$/).min(3).required(),
-    dni: Joi.string().pattern(/^\d+$/).min(8).required(),
-    phone: Joi.string().min(10),
+    dni: Joi.string().pattern(/^\d+$/).min(8).required()
+      .max(10),
+    phone: Joi.string().min(10).max(12),
     email: Joi.string().email().required(),
-    password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/).min(8).required(),
-    salary: Joi.string(),
+    password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/).min(8).max(20)
+      .required(),
+    salary: Joi.string().regex(/^\$\d+(?:\.\d+)?$/).max(10000000).min(1),
     isActive: Joi.boolean(),
   });
 
@@ -28,11 +30,11 @@ const validateTrainerUpdate = (req, res, next) => {
   const trainerValidation = Joi.object({
     firstName: Joi.string().pattern(/^[a-zA-Z\s]+$/).min(3),
     lastName: Joi.string().pattern(/^[a-zA-Z\s]+$/).min(3),
-    dni: Joi.string().pattern(/^\d+$/).min(8),
+    dni: Joi.string().pattern(/^\d+$/).min(8).max(10),
     phone: Joi.string().pattern(/^\d+$/),
     email: Joi.string().email(),
-    password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/).min(8),
-    salary: Joi.string(),
+    password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/).min(8).max(20),
+    salary: Joi.string().regex(/^\$\d+(?:\.\d+)?$/).max(10000000).min(1),
     isActive: Joi.boolean(),
   });
   const validation = trainerValidation.validate(req.body);
