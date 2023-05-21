4ed879161bc9e3b580a1f4232b3a1cb8ec54e250
Cristian Zufiaurre <cjzufi@gmail.com>
2023-05-21 20:04:15 -0300

    MR-24: tests for the get method

--- src/tests/super-admins.test.js
+++ src/tests/super-admins.test.js
@@ -1,5 +1,20 @@
-// Basic test to avoid conflicts
-// REMOVE THIS TEST AND MAKE YOURS!
-test('should pass a basic test', () => {
-  expect(1 + 1).toBe(2);
+import request from 'supertest';
+import app from '../app';
+import SuperAdmin from '../models/SuperAdmin';
+import superAdminSeed from '../seeds/super-admins';
+
+beforeAll(async () => {
+  await SuperAdmin.collection.insertMany(superAdminSeed);
+});
+
+describe('Get all super admins tests ', () => {
+  test('On good request should return status 200', async () => {
+    const response = await request(app).get('/api/super-admins/').send();
+    expect(response.status).toBe(200);
+    expect(response.error).toBeFalsy();
+  });
+  test('On good request should return full super admins list', async () => {
+    const response = await request(app).get('/api/super-admins/').send();
+    expect(response.body.data.length).toEqual(superAdminSeed.length);
+  });
 });
