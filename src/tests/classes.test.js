779c68b12fbbdec0c17fdf20af7bcca68a04bbcb
Cristian Lotorto <cristian.lotorto@gmail.com>
2023-05-21 17:03:10 -0300

    MR-35: update endpoint tests

--- src/tests/classes.test.js
+++ src/tests/classes.test.js
@@ -8,51 +8,61 @@ beforeAll(async () => {
   await Class.collection.insertMany(classSeed);
 });
 
-// const mockUpdatedClass = {
-//   activity: {
-//     _id: '6465286dad795d0bf31704f1',
-//     name: 'Sorcery',
-//     description: 'throw spells and that kind of stuff.',
-//     isActive: true,
-//   },
-//   trainer: [
-//     {
-//       _id: '6460763768fd665d7bf97f13',
-//       firstName: 'Harald',
-//       lastName: 'Potterson',
-//       dni: '06266798',
-//       phone: '1221221122',
-//       email: 'hpoter3@imgur.com',
-//       password: 'tvqTPiNMMMMMBb1',
-//       salar: '$170000',
-//       isActive: true,
-//     },
-//   ],
-//   day: 'Monday',
-//   time: '19:10',
-//   capacity: 100000000000000,
-//   deleted: true,
-// };
-
 describe('PUT logic delete by id api/class/delete/:id', () => {
-  test('return status 200 - "delete" property change false for true', async () => {
+  test('on good request return status 200 - "delete" property change false for true', async () => {
     const response = await request(app).put(`/api/class/delete/${classSeed[0]._id}`).send();
     expect(response.status).toBe(200);
     expect(response.error).toBeFalsy();
     expect(response._body.deleted_class.deleted).toBe(true);
   });
-  test('return status 404', async () => {
+  test('when does not exists return status 404', async () => {
     const response = await request(app).put(`/api/class/delete/${classSeed[0]._id}`).send();
     expect(response.status).toBe(404);
     expect(response.error).toBeTruthy();
   });
-  test('return status 400', async () => {
+  test('on bad request return status 400', async () => {
     const response = await request(app).put('/api/class/delete/lalalalalalaa').send();
     expect(response.status).toBe(400);
     expect(response.error).toBeTruthy();
   });
-  test('the deleted object is restored', async () => {
+  test('deleted object is restored', async () => {
     const restore = await request(app).put(`/api/class/restore/${classSeed[0]._id}`).send();
     expect(restore._body.restored_class.deleted).toBe(false);
+  });
+});
+
+describe('PUT update by id api/class/:id', () => {
+  test('return status 200 - error false - updates only the passed props', async () => {
+    const classBefore = classSeed[1];
+    const classAfter = await request(app).put(`/api/class/${classSeed[0]._id}`).send({ trainer: classSeed[1].trainer._id });
+    expect(classAfter.status).toBe(200);
+    expect(classAfter.error).toBeFalsy();
+    expect(classAfter._body.class.day).toBe(classBefore.day);
+  });
+  test('when does not exists return status 404', async () => {
+    const response = await request(app).put('/api/class/6462ea9abb5168b3bbbc8bc5').send({ trainer: classSeed[1].trainer._id });
+    expect(response.status).toBe(404);
+    expect(response.error).toBeTruthy();
+  });
+  test('on bad request return status 400', async () => {
+    const response = await request(app).put(`/api/class/${classSeed[0]._id}`).send({ trainer: classSeed[1].activity._id });
+    expect(response.status).toBe(400);
+    expect(response.error).toBeTruthy();
+  });
+  test('throws bad request when try to change "deleted" prop', async () => {
+    const response = await request(app).put(`/api/class/${classSeed[0]._id}`).send({ deleted: true });
+    expect(response.status).toBe(400);
+    expect(response.error).toBeTruthy();
+    expect(response._body.message).toBe('There was an error "deleted" is not allowed');
+  });
+  test('if do not meets the length validation throws error', async () => {
+    const response = await request(app).put(`/api/class/${classSeed[0]._id}`).send({ activity: 'aaaaaaaaa123' });
+    expect(response.status).toBe(400);
+    expect(response.error).toBeTruthy();
+  });
+  test('if do not meets the data type validation throws error', async () => {
+    const response = await request(app).put(`/api/class/${classSeed[0]._id}`).send({ time: 'lalala' });
+    expect(response.status).toBe(400);
+    expect(response.error).toBeTruthy();
   });
 });
