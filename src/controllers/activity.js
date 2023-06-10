ce5ac783c245e15578570ed8e061690b7d4e522b
francoax <francoa.duarte2001@gmail.com>
2023-06-10 20:43:02 -0300

    MR-88: comments on pr done.

--- src/controllers/activity.js
+++ src/controllers/activity.js
@@ -39,7 +39,7 @@ const deleteActivity = (req, res) => {
       });
     })
     .catch((error) => res.status(400).json({
-      message: 'An error ocurred!',
+      message: 'An error ocurred',
       data: undefined,
       error,
     }));
@@ -54,7 +54,7 @@ const getAllActivity = (req, res) => {
     }))
     .catch((error) => {
       res.status(500).json({
-        message: 'An error ocurred!',
+        message: 'An error ocurred',
         data: undefined,
         error,
       });
@@ -66,7 +66,7 @@ const getActivityById = (req, res) => {
 
   Activity.findById(id)
     .then((activity) => res.status(200).json({
-      message: 'Activity found!',
+      message: 'Activity found',
       data: activity,
       error: false,
     }))
