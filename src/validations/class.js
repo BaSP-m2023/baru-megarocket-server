46ce78df285576f871390a8c822764e3be6e213f
Ignacio <igna.galceran@gmail.com>
2023-06-11 17:20:32 -0300

    MR-88: deleted method created, validations added

--- src/validations/class.js
+++ src/validations/class.js
@@ -3,10 +3,11 @@ const Joi = require('joi');
 const validateCreation = (req, res, next) => {
   const classValidation = Joi.object({
     activity: Joi.string().hex().length(24).required(),
-    trainer: Joi.array().items(Joi.string().hex().length(24)).required(),
+    trainer: Joi.string().hex().length(24).required(),
     day: Joi.string().regex(/^(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)$/).required(),
     time: Joi.string().regex(/^([0-9]|[01]\d|2[0-3]):([0-5]\d)$/).required(),
-    capacity: Joi.number().min(1).required(),
+    capacity: Joi.number().min(1).max(50).required()
+      .integer(),
   });
 
   const validation = classValidation.validate(req.body);
@@ -55,10 +56,10 @@ const validateAssignActivity = (req, res, next) => {
 const validateUpdate = (req, res, next) => {
   const classValidation = Joi.object({
     activity: Joi.string().hex().length(24),
-    trainer: Joi.array().items(Joi.string().hex().length(24)),
+    trainer: Joi.string().hex().length(24),
     day: Joi.string().regex(/^(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)$/),
     time: Joi.string().regex(/^([0-9]|[01]\d|2[0-3]):([0-5]\d)$/),
-    capacity: Joi.number().min(1),
+    capacity: Joi.number().min(1).max(50).integer(),
   });
 
   const validation = classValidation.validate(req.body);
