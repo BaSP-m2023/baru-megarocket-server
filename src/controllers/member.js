06027b51d30f9d4bbc772fe17152516b330a367b
FontanR <r.fontan12@gmail.com>
2023-06-24 18:12:59 -0300

    MR-155: Implement firebase, change schema and controllers

--- src/controllers/member.js
+++ src/controllers/member.js
@@ -1,4 +1,7 @@
 const Member = require('../models/Member');
+const { default: firebaseApp } = require('../helper/firebase');
+
+const regexObjectId = /^[0-9a-fA-F]{24}$/;
 
 const getAllMembers = (req, res) => {
   Member.find()
@@ -7,14 +10,24 @@ const getAllMembers = (req, res) => {
       data: member,
       error: false,
     }))
-    .catch((error) => res.status(500).json({
+    .catch(() => res.status(500).json({
       message: 'Unexpected error. Please try again later.',
-      error,
+      data: undefined,
+      error: true,
     }));
 };
 
+// eslint-disable-next-line consistent-return
 const getMemberById = (req, res) => {
   const { id } = req.params;
+
+  if (!id.match(regexObjectId)) {
+    return res.status(404).json({
+      message: 'Please put a valid ID',
+      data: undefined,
+      error: true,
+    });
+  }
 
   Member.findById(id)
     .then((member) => {
@@ -24,59 +37,94 @@ const getMemberById = (req, res) => {
         error: false,
       });
     })
-    .catch((error) => {
-      res.status(404).json({
+    .catch(() => {
+      res.status(500).json({
         message: `The member with the id ${id} does not exist`,
-        error,
+        data: undefined,
+        error: true,
       });
     });
 };
 
-const createMember = (req, res) => {
+const createMember = async (req, res) => {
   const {
-    name, lastName, phone, dni, city, dob, zip, isActive, membership, email, password,
+    name,
+    lastName,
+    phone, dni,
+    city,
+    dob,
+    zip,
+    isActive,
+    membership,
+    email,
   } = req.body;
 
-  Member.create({
-    name, lastName, phone, dni, city, dob, zip, isActive, membership, email, password,
+  const memberExists = await Member.findOne({ email });
+  if (memberExists) {
+    return res.status(400).json({
+      message: 'EMail is already in use by another member',
+      data: undefined,
+      error: true,
+    });
+  }
+
+  const newFirebaseUser = await firebaseApp.auth().createUser({
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  const firebaseUid = newFirebaseUser.uid;
+  await firebaseApp.auth().setCustomUserClaims(newFirebaseUser.uid, { role: 'MEMBER' });
+
+  return Member.create({
+    firebaseUid,
+    name,
+    lastName,
+    phone,
+    dni,
+    city,
+    dob,
+    zip,
+    isActive,
+    membership,
+    email,
   })
     .then((result) => res.status(201).json({
       message: 'Member created',
       data: result,
       error: false,
     }))
-    .catch((error) => res.status(400).json({
-      message: 'Invalid Request: Incorrect parameters provided.',
-      error,
+    .catch(() => res.status(500).json({
+      message: 'Invalid Request: Internal error',
+      data: undefined,
+      error: true,
     }));
 };
 
-const deleteMember = (req, res) => {
+// eslint-disable-next-line consistent-return
+const updateMember = async (req, res) => {
   const { id } = req.params;
-  Member.findByIdAndDelete(id)
-    .then((result) => {
-      if (!result) {
-        return res.status(404).json({
-          message: `Member with id ${id} not found`,
-          error: true,
-        });
-      }
-      return res.status(200).json({
-        message: 'Member deleted',
-        data: result,
-        error: false,
-      });
-    })
-    .catch((error) => res.status(400).json({
-      message: 'Something went wrong',
-      error,
-    }));
-};
-const updateMember = (req, res) => {
-  const { id } = req.params;
+
+  if (!id.match(regexObjectId)) {
+    return res.status(404).json({
+      message: 'Please put a valid ID',
+      data: undefined,
+      error: true,
+    });
+  }
+
   const {
-    name, lastName, dni, phone, email, city, dob, zip, isActive, membership, password,
+    name,
+    lastName,
+    dni,
+    phone,
+    city,
+    dob,
+    zip,
+    isActive,
+    membership,
   } = req.body;
+
   Member.findByIdAndUpdate(
     id,
     {
@@ -84,13 +132,11 @@ const updateMember = (req, res) => {
       lastName,
       dni,
       phone,
-      email,
       city,
       dob,
       zip,
       isActive,
       membership,
-      password,
     },
     { new: true },
   )
@@ -98,6 +144,7 @@ const updateMember = (req, res) => {
       if (!result) {
         return res.status(404).json({
           message: `Member with id ${id} was not found`,
+          data: undefined,
           error: true,
         });
       }
@@ -107,9 +154,43 @@ const updateMember = (req, res) => {
         error: false,
       });
     })
-    .catch((error) => res.status(400).json({
+    .catch(() => res.status(500).json({
       message: 'Something went wrong',
-      error,
+      data: undefined,
+      error: true,
+    }));
+};
+
+const deleteMember = (req, res) => {
+  const { id } = req.params;
+
+  if (!id.match(regexObjectId)) {
+    res.status(404).json({
+      message: 'Please put a valid ID',
+      data: undefined,
+      error: true,
+    });
+  }
+
+  Member.findByIdAndDelete(id)
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          message: `Member with id ${id} not found`,
+          data: undefined,
+          error: true,
+        });
+      }
+      return res.status(200).json({
+        message: 'Member deleted',
+        data: result,
+        error: false,
+      });
+    })
+    .catch(() => res.status(500).json({
+      message: 'Something went wrong',
+      data: undefined,
+      error: true,
     }));
 };
 
