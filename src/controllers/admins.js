705e54631d7830c2d72de214c4ace5d5550e11b8
francoax <francoa.duarte2001@gmail.com>
2023-06-24 18:08:30 -0300

    MR-153: firebase implemented in controller and admins model.

--- src/controllers/admins.js
+++ src/controllers/admins.js
@@ -1,59 +1,112 @@
+const mongoose = require('mongoose');
+const { default: firebaseApp } = require('../helper/firebase/index');
+
 const Admin = require('../models/Admin');
 const { validateUpdate } = require('../validations/admins');
 
-const createAdmin = (req, res) => {
+const createAdmin = async (req, res) => {
   const {
-    firstName, lastName, dni, phone, email, city, password,
+    firstName, lastName, dni, phone, email, city,
   } = req.body;
-  Admin.create({
-    firstName,
-    lastName,
-    dni,
-    phone,
-    email,
-    city,
-    password,
-  })
-    .then((result) => {
-      res.status(201).json({
-        message: 'Admin created',
-        data: result,
-        error: false,
-      });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        message: 'An error ocurred!',
-        error: error.message,
-      });
+  let fireBaseUid;
+
+  try {
+    const adminExists = await Admin.findOne({ $or: [{ dni }, { email }] });
+    if (adminExists) {
+      if (adminExists.dni === dni) {
+        return res.status(400).json({
+          message: 'There is another admin with that dni.',
+          data: undefined,
+          error: true,
+        });
+      }
+      if (adminExists.email === email) {
+        return res.status(400).json({
+          message: 'There is another admin with that email.',
+          data: undefined,
+          error: true,
+        });
+      }
+    }
+
+    const newFirebaseUser = await firebaseApp.auth().createUser({
+      email: req.body.email,
+      password: req.body.password,
     });
+    fireBaseUid = newFirebaseUser.uid;
+
+    await firebaseApp.auth().setCustomUserClaims(newFirebaseUser.uid, { role: 'ADMIN' });
+
+    const admin = new Admin({
+      fireBaseUid,
+      firstName,
+      lastName,
+      dni,
+      phone,
+      email,
+      city,
+    });
+
+    const adminsaved = await admin.save();
+
+    return res.status(201).json({
+      message: 'Admin created',
+      data: adminsaved,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: 'An error ocurred!',
+      error: error.message,
+    });
+  }
 };
 
-const getAllAdmins = (req, res) => {
-  Admin.find({ deleted: false })
-    .then((admins) => res.status(200).json({
-      data: admins,
-      error: false,
-    }))
-    .catch((error) => res.status(500).json({
-      message: 'An error ocurred',
-      error: error.message,
-    }));
+const getAllAdmins = async (req, res) => {
+  try {
+    const admins = await Admin.find();
+
+    if (admins.length > 0) {
+      res.status(200).json({
+        message: 'Admins list',
+        data: admins,
+        error: false,
+      });
+    }
+
+    if (admins.length === 0) {
+      res.status(404).json({
+        message: 'There are not admins yet.',
+        data: [],
+        error: false,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'An error has ocurred',
+      error,
+    });
+  }
 };
 
 const getAdminById = (req, res) => {
   const { id } = req.params;
 
-  Admin.findById(id)
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({
+      message: 'Invalid id',
+      data: id,
+      error: true,
+    });
+  }
+
+  return Admin.findById(id)
     .then((admin) => {
       if (!admin) {
         return res.status(404).json({
           message: 'Admin not found',
-        });
-      }
-      if (admin.deleted === true) {
-        return res.status(200).json({
-          message: 'Admin was deleted',
+          data: undefined,
+          error: true,
         });
       }
       return res.status(200).json({
@@ -62,7 +115,7 @@ const getAdminById = (req, res) => {
         error: false,
       });
     })
-    .catch((error) => res.status(400).json({
+    .catch((error) => res.status(500).json({
       message: 'An error occurred',
       error: error.message,
     }));
@@ -71,24 +124,30 @@ const getAdminById = (req, res) => {
 const deleteAdmin = (req, res) => {
   const { id } = req.params;
 
-  Admin.findByIdAndUpdate(id, { deleted: true })
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({
+      message: 'Invalid id',
+      data: id,
+      error: true,
+    });
+  }
+
+  return Admin.findByIdAndDelete(id)
     .then((admin) => {
       if (!admin) {
         return res.status(404).json({
           message: `Admin with id: ${id} was not found!`,
-        });
-      }
-      if (admin.deleted) {
-        return res.status(400).json({
-          message: `Admin with id: ${id} has already been deleted!`,
+          data: undefined,
+          error: true,
         });
       }
       return res.status(200).json({
         message: 'Admin deleted',
-        deleted: true,
+        data: admin,
+        error: false,
       });
     })
-    .catch((error) => res.status(400).json({
+    .catch((error) => res.status(500).json({
       message: 'An error ocurred',
       error: error.message,
     }));
@@ -103,7 +162,27 @@ const updateAdmin = (req, res) => {
   if (!firstName && !lastName && !dni && !phone && !email && !city && !password) {
     return res.status(400).json({
       message: 'At least one field should be modified',
+      data: req.body,
+      error: true,
     });
+  }
+
+  const adminExists = Admin.findOne({ $or: [{ dni }, { email }] });
+  if (adminExists) {
+    if (adminExists.dni === dni) {
+      return res.status(400).json({
+        message: 'There is another admin with that DNI.',
+        data: undefined,
+        error: true,
+      });
+    }
+    if (adminExists.email === email) {
+      return res.status(400).json({
+        message: 'There is another admin with that email.',
+        data: undefined,
+        error: true,
+      });
+    }
   }
 
   return validateUpdate(req, res, () => {
@@ -116,7 +195,6 @@ const updateAdmin = (req, res) => {
         phone,
         email,
         city,
-        password,
       },
       {
         new: true,
@@ -126,6 +204,8 @@ const updateAdmin = (req, res) => {
         if (!result) {
           return res.status(404).json({
             message: `Admin with id: ${id} was not found!`,
+            data: undefined,
+            error: true,
           });
         }
         return res.status(200).json({
@@ -134,54 +214,11 @@ const updateAdmin = (req, res) => {
           error: false,
         });
       })
-      .catch((error) => res.status(400).json({
+      .catch((error) => res.status(500).json({
         message: 'An error occurred',
         error: error.message,
       }));
   });
-};
-
-const recoverAdmin = (req, res) => {
-  const { id } = req.params;
-
-  Admin.findByIdAndUpdate(id, { deleted: false })
-    .then((admin) => {
-      if (!admin) {
-        return res.status(404).json({
-          message: `Admin with id: ${id} was not found!`,
-        });
-      }
-      if (!admin.deleted) {
-        return res.status(400).json({
-          message: `Admin with id: ${id} has never been deleted!`,
-        });
-      }
-      return res.status(200).json({
-        message: `Admin with id: ${id} was successfully recovered!`,
-        data: admin,
-        deleted: false,
-      });
-    })
-    .catch((error) => res.status(400).json({
-      message: 'An error ocurred',
-      error: error.message,
-    }));
-};
-
-const cleanAdmins = (req, res) => {
-  Admin.deleteMany({ deleted: true })
-    .then((result) => {
-      if (result.n === 0) {
-        return res.status(404).json({
-          message: 'The DB have not admins to clean',
-        });
-      }
-      return res.status(204).end();
-    })
-    .catch((error) => res.status(400).json({
-      message: 'An error ocurred',
-      error: error.message,
-    }));
 };
 
 module.exports = {
@@ -190,6 +227,4 @@ module.exports = {
   updateAdmin,
   createAdmin,
   deleteAdmin,
-  recoverAdmin,
-  cleanAdmins,
 };
