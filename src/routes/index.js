ad98a7b8b7bb6a5ff69f718de951c3b33f8ca229
Walter Ariel Pavicich <walterarielpavicich@gmail.com>
2023-06-25 19:15:42 -0300

    MR-166: import middleware to index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,17 +8,18 @@ const admins = require('./admins');
 const activity = require('./activity');
 const classes = require('./class');
 const auth = require('./auth');
+const { default: verifyToken } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.use('/subscription', subscriptions);
-router.use('/trainer', trainers);
-router.use('/admins', admins);
+router.use('/trainer', verifyToken, trainers);
+router.use('/admins', verifyToken, admins);
 router.use('/activities', activity);
 router.use('/class', classes);
-router.use('/member', members);
-router.use('/super-admins', superAdmins);
+router.use('/member', verifyToken, members);
+router.use('/super-admins', verifyToken, superAdmins);
 router.use('/subscription', subscriptions);
-router.use('/auth', auth);
+router.use('/auth', verifyToken, auth);
 
 module.exports = router;
